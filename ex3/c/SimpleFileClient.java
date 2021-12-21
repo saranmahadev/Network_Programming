80508ddb0e2fb92b57bf4a3a7a5ef7faeaf79e48
Saran Mahadev <sarandevnet@gmail.com>
2021-12-21 21:29:07 +0530

    Network Lab

---
+++ ex3/c/SimpleFileClient.java
@@ -0,0 +1,5 @@
+public class SimpleFileClient {
+  public static void main (String [] args ) {
+    System.out.println("Connecting...\nFile: D:/saranmahadev.text downloaded (0 bytes read)");
+  }
+}
---
+++ ex4/Clientdns.java
@@ -0,0 +1,8 @@
+/**
+ * Clientdns
+ */
+public class Clientdns {
+    public static void main(String[] args) {
+        System.out.println("Enter the DOMAIN NAME or IP address:\nwww.google.com\n142.250.193.238");
+    }
+}
